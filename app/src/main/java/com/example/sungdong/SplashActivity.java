0a4aeb211ab0361324601a42fbde13fbfb3f59f3
snowplum212 <snowplum212@gmail.com>
2023-09-21 00:16:05 +0900

    Splash animation add

--- app/src/main/java/com/example/sungdong/SplashActivity.java
+++ app/src/main/java/com/example/sungdong/SplashActivity.java
@@ -3,21 +3,31 @@ package com.example.sungdong;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static final long SPLASH_DELAY = 2000;
+    private static final long SPLASH_DELAY = 3000;
+    ImageView iv_splash_logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        iv_splash_logo = findViewById(R.id.iv_splash_logo);
+        Animation fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        iv_splash_logo.startAnimation(fadeInAnimation);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                Animation fadeOutAnimation = AnimationUtils.loadAnimation(SplashActivity.this, R.anim.fade_out);
+                iv_splash_logo.startAnimation(fadeOutAnimation);
                 Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(mainIntent);
                 finish();
